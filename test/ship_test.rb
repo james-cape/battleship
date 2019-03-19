c9a41bd32e2a820b79023d928d7c79a1dfffeb42
James Cape <james.cape@gmail.com>
2019-03-18 20:47:53 -0600

    Write ship tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -11,10 +11,74 @@ class ShipTest < Minitest::Test
   end
 
   def test_ship_exists
-  expected = Ship
-  actual = @cruiser
-  assert_instance_of expected, actual
+
+    expected = Ship
+    actual = @cruiser #check if we need the "@" once we finish
+    assert_instance_of expected, actual
   end
 
+  def test_ship_has_a_name
+
+    expected = "Cruiser"
+    actual = cruiser.name
+    assert_equal expected, actual
+  end
+
+  def test_ship_has_a_length
+
+    expected = 3
+    actual = cruiser.length
+    assert_equal expected, actual
+  end
+
+  def test_ship_starts_with_3_health
+
+    expected = 3
+    actual = cruiser.health
+    assert_equal expected, actual
+  end
+
+  def test_ship_starts_not_sunk
+
+    expected = false
+    actual = cruiser.sunk?
+    assert_equal expected, actual
+  end
+
+  def test_ship_health_after_one_hit
+    cruiser.hit
+
+    expected = 2
+    actual = cruiser.health
+    assert_equal expected, actual
+  end
+
+  def test_ship_health_after_two_hits
+    cruiser.hit
+    cruiser.hit
+
+    expected = 1
+    actual = cruiser.health
+    assert_equal expected, actual
+  end
+
+  def test_ship_not_sunk_after_two_hits
+    cruiser.hit
+    cruiser.hit
+
+    expected = false
+    actual = cruiser.sunk
+    assert_equal expected, actual
+  end
+
+  def test_ship_sunk_after_three_hits
+    cruiser.hit
+    cruiser.hit
+    cruiser.hit
+
+    expected = true
+    actual = cruiser.sunk?
+    assert_equal expected, actual
+  end
 
 end
