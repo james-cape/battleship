9fe1d41765f89ca9fc6bc8cc7837b1ac2b61272e
James Cape <james.cape@gmail.com>
2019-03-20 12:21:58 -0600

    Write all tests for Iteration 2

--- test/board_test.rb
+++ test/board_test.rb
@@ -109,28 +109,28 @@ class BoardTest < Minitest::Test
 #### Validating Placements
   def test_number_of_coordinates_is_same_as_ship_length
     skip
-    assert_equal false, @board.valid_placement?(cruiser, ["A1", "A2"])
-    assert_equal false, @board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 
   def test_coordinates_are_consecutive
     skip
-    assert_equal false, @board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    assert_equal false, @board.valid_placement?(submarine, ["A1", "C1"])
-    assert_equal false, @board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    assert_equal false, @board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
   end
 
   def test_coordinates_can_not_be_diagonal
     skip
-    assert_equal false, @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-    assert_equal false, @board.valid_placement?(submarine, ["C2", "D3"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
   def test_previous_checks_pass_meaning_placement_should_be_valid
     skip
-    assert_equal true, @board.valid_placement?(submarine, ["A1", "A2"])
-    assert_equal true, @board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 
   #### Ranges
@@ -196,7 +196,91 @@ class BoardTest < Minitest::Test
 # The Range with Strings works because each character has an implicit value that tells us in what order the characters should be. This is called the Ordinal Value, and you can access it with the ord method for Strings:
 
   def test_verify_ordinal_value
-
+    skip
+    expected = 65
+    actual = "A".ord
+    assert_equal expected, actual
   end
+
+  def test_verify_another_ordinal_value
+    skip
+    expected = 68
+    actual = "D".ord
+    assert_equal expected, actual
+  end
+
+#### Helpful Enumerables
+## Ruby has an enumerable method called each_cons that allows you to access consecutive elements in a collection. See the Ruby Docs for more details.
+##
+## Also consider if the enumerables any?, all?, none? would be helpful.
+
+
+#### Placing Ships
+  def test_same_ship_is_on_cell_2_and_cell_3
+    skip
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    @cell_1.ship
+    @cell_2.ship
+    @cell_3.ship
+
+    expected = true
+    actual = @cell_3.ship == @cell_2.ship
+    assert_equal expected, actual
+  end
+
+#### Overlapping Ships
+  def test_ships_cannot_overlap
+    skip
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    @cell_1.ship
+    @cell_2.ship
+    @cell_3.ship
+
+    expected = false
+    actual = @board.valid_placement?(@submarine, ["A1", "B1"])
+    assert_equal expected, actual
+  end
+
+#### Rendering the Board
+## Rendering the Board
+## Our board needs to be able to render a String representation of itself to display to the user all of its cells in a formatted grid. Each Cell should be displayed using the rules from Iteration 1:
+##
+## ”.” if the cell has not been fired upon.
+## “M” if the cell has been fired upon and it does not contain a ship (the shot was a miss).
+## “H” if the cell has been fired upon and it contains a ship (the shot was a hit).
+## “X” if the cell has been fired upon and its ship has been sunk. Note that all of the cells that contain that sunken ship should render as an “X”, not just the cell that resulted in the ship being sunk.
+## And just like with cells, we will include an optional argument to indicate whether we want to show hidden ships.
+
+  def test_board_rendering
+    skip
+    @board.place(cruiser, ["A1", "A2", "A3"])
+
+    expected =  "  1 2 3 4 \n" +
+                "A . . . . \n" +
+                "B . . . . \n" +
+                "C . . . . \n" +
+                "D . . . . \n"
+
+    actual = @board.render
+    assert_equal expected, actual
+  end
+
+  def test_board_rendering_with_optional_true____the_S
+    skip
+    @board.place(cruiser, ["A1", "A2", "A3"])
+
+    expected =  "  1 2 3 4 \n" +
+                "A S S S . \n" +
+                "B . . . . \n" +
+                "C . . . . \n" +
+                "D . . . . \n"
+
+    actual = @board.render(true)
+    assert_equal expected, actual
+  end
+
+#### End of Iteration 2.
+
+
 
 end
