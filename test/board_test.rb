2972fbeb67939b314b5a3e3782f47eb9d42591fb
Ryan Miller <ryanmillergm@gmail.com>
2019-03-20 17:47:11 -0600

    Pass initial board setup tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -10,61 +10,37 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
-    @coordinates_array = [
-              "A1", "A2", "A3", "A4",   # For now I'm hard coding
-              "B1", "B2", "B3", "B4",
-              "C1", "C2", "C3", "C4",
-              "D1", "D2", "D3", "D4"   ]
-
-    @cell_array = [
-      cell_1 = Cell.new("A1"),
-      cell_2 = Cell.new("A2"),
-      cell_3 = Cell.new("A3"),
-      cell_4 = Cell.new("A4"),
-      cell_5 = Cell.new("B1"),
-      cell_6 = Cell.new("B2"),
-      cell_7 = Cell.new("B3"),
-      cell_8 = Cell.new("B4"),
-      cell_9 = Cell.new("C1"),
-      cell_10 = Cell.new("C2"),
-      cell_11 = Cell.new("C3"),
-      cell_12 = Cell.new("C4"),
-      cell_13 = Cell.new("D1"),
-      cell_14 = Cell.new("D2"),
-      cell_15 = Cell.new("D3"),
-      cell_16 = Cell.new("D4")    ]
-
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @range = 3..8
-
   end
 
   def test_board_exists
-    skip
+
     expected = Board
-    actual = @board_hash
+    actual = @board
     assert_instance_of expected, actual
   end
 
-  def test_board_hash_has_16_key_value_pairs
+  def test_board_has_16_key_value_pairs
+
     expected = 16
-    actual = @board_hash.length
+    actual = @board.cells.length
     #.length refers to key:value pairs, not the board length.
     assert_equal expected, actual
   end
 
 
   def test_board_is_a_hash #using board.kind_of?(Hash)
-    skip
-    expected = True
-    actual = @board_hash.kind_of?(Hash)
+
+    expected = true
+    actual = @board.cells.kind_of?(Hash)
     assert_equal expected, actual
   end
 
 
-  def test_all_16_board_hash_keys_point_to_cell_objects
-    skip
+  def test_all_16_board_keys_point_to_cell_objects
+    
     expected = true
 
     actual = @board.values.all? do |val|
@@ -76,7 +52,7 @@ class BoardTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  def test_first_key_value_pair_in_board_hash_is_A1
+  def test_first_key_value_pair_in_board_is_A1
     skip
 
 ##### I accidentally wrote the method - just copy paste this into the ship.rb to solve this test.
