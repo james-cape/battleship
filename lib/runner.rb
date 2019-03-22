8f8c93a066747c6fcb5d453da6a46996e6d035f9
James Cape <james.cape@gmail.com>
2019-03-22 12:40:45 -0600

    Add computer and runner files. Establish a few tests for Computer

---
+++ lib/runner.rb
@@ -0,0 +1,7 @@
+puts "Welcome to BATTLESHIP"
+puts "Enter p to play. Enter q to quit."
+
+play_or_quit = gets.chomp
+
+# if play_or_quit = "p", computer places ships
+# if play_or_quit = "q", program ends/exits.
