88d37775a82969a4b45cb01b7bbd537694192e3e
Ryan Miller <ryanmillergm@gmail.com>
2019-03-22 17:57:35 -0600

    Create random computer cell for ship placement check

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,19 +1,49 @@
+require './lib/ship'
+
 class Computer
 
-  def initialize(board, ships)
-    # @board = Board.new
-    # @ships = []
+
+  def initialize(board, ship)
+    @board = board
+    @ship = ship
+    @ships_array = []
+  end
+
+  def ships_array
+    @ships_array << @ship
+    # binding.pry
   end
 
   def pick_random_cell
+    @board.cells.keys.sample
+  end
+
+  def random_cardinal_direction
+    [1, 2, 3, 4].sample
+  end
+
+  def feed_ships
+    [@cruiser = Ship.new("Cruiser", 3), @submarine = Ship.new("Submarine", 2)].each do |ship|
+      #Pick a random allowable row/column 
 
   end
 
-
-  def place_random_ship
-  end
-
-
-
+  # def generate_cell_names_with_ship
+  #   ship_placed_on = []
+  #   originator_cell = pick_random_cell
+  #
+  #   ship_placed_on << originator_cell
+  #
+  #   # originator_cell.split("")
+  #   if random_cardinal_direction.odd?
+  #     # binding.pry
+  #     # shiplength each do |unit|
+  #     #
+  #     #   originator_cell.split("")[0].ord - 1
+  #   else
+  #     binding.pry
+  #   end
+  #
+  # end
 
 end
--- test/computer_test.rb
+++ test/computer_test.rb
@@ -10,42 +10,53 @@ class ComputerTest < Minitest::Test
 
   def setup
     @board = Board.new
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
-    @computer = Computer.new
+
+
+    @computer_ships = []
+    @computer = Computer.new(@board, @computer.ships_array)
   end
 
   def test_board_exists
-    skip
     expected = Board
     actual = @board
     assert_instance_of expected, actual
   end
 
-  def test_cruiser_exists
-    skip
-    expected = Ship
-    actual = @cruiser
-    assert_instance_of expected, actual
-  end
+  # def test_cruiser_exists
+  #   expected = Ship
+  #   actual = @cruiser
+  #   assert_instance_of expected, actual
+  # end
 
   def test_computer_exists
-    skip
     expected = Computer
     actual = @computer
     assert_instance_of expected, actual
   end
+
+  def test_computer_random_cell
+    assert_instance_of String, @computer.pick_random_cell
+  end
+
+  def test_random_cardinal_direction
+    assert_instance_of Integer, @computer.random_cardinal_direction
+  end
+
+
+
+  def test_extends_cells_to_length_of_ship
+    expected = Array
+    actual = @computer.generate_cell_names_with_ship
+    assert_instance_of expected, actual
+  end
+
+
 
   def test_computer_can_place_random_ships
     skip
     expected =
     actual =
     assert_instance_of expected, actual
-
   end
-
-
-
-
 
 end
