2fd7ff081885f40d9817d164048112e44333c13e
James Cape <james.cape@gmail.com>
2019-03-24 17:13:01 -0600

    Clean up

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,59 +1,47 @@
-require './lib/ship'
-
 class Computer
-
 
   def initialize(board, ships)
     @board = board
     @computer_ships = ships
-
-
   end
 
   def feed_ships
     @computer_ships.each do |ship|
 
-      possible_horizontal_configurations = []
-      possible_ship_footprint = []
+      possible_horizontal_footprints = []
       @board.row_combos(ship).each do |row_combo|
         row_combo.each do |row|
           @board.column_combos(ship).each do |col_combo|
-            col_combo.each do |col|
-              possible_ship_footprint << row + col
+            possible_ship_footprint = col_combo.map do |col|
+              row + col
             end
-            possible_horizontal_configurations << possible_ship_footprint
+            possible_horizontal_footprints << possible_ship_footprint
             possible_ship_footprint = []
           end
         end
       end
-      y = possible_horizontal_configurations.uniq
 
-      possible_vertical_configurations = []
-      possible_ship_footprint = []
-
+      possible_vertical_footprints = []
       @board.column_combos(ship).each do |col_combo|
         col_combo.each do |col|
           @board.row_combos(ship).each do |row_combo|
-            row_combo.each do |row|
-              possible_ship_footprint << row + col
+            possible_ship_footprint = row_combo.map do |row|
+              row + col
             end
-            possible_vertical_configurations << possible_ship_footprint
+            possible_vertical_footprints << possible_ship_footprint
             possible_ship_footprint = []
           end
         end
       end
-      x = possible_vertical_configurations.uniq
-      all_combos = x + y
+
+      all_combos = (possible_horizontal_footprints + possible_vertical_footprints).uniq
 
       has_overlapping = true
       while has_overlapping == true
-
         ship_footprint = all_combos.sample
         has_overlapping = ship_footprint.any? do |cell|
-          # binding.pry
           @board.cells[cell].empty? == false
         end
-
       end
       @board.place(ship, ship_footprint)
     end
