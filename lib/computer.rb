0742a06014c880897f2a1075a11e0f8719477de7
James Cape <james.cape@gmail.com>
2019-03-25 17:24:00 -0600

    Complete helper methods

--- lib/computer.rb
+++ lib/computer.rb
@@ -21,32 +21,38 @@ class Computer
     possible_vertical_footprints
   end
 
+  def possible_horizontal_footprints(ship)
+    possible_horizontal_footprints = []
+    @board.row_combos(ship).each do |row_combo|
+      row_combo.each do |row|
+        @board.column_combos(ship).each do |col_combo|
+          possible_ship_footprint = col_combo.map do |col|
+            row + col
+          end
+          possible_horizontal_footprints << possible_ship_footprint
+          possible_ship_footprint = []
+        end
+      end
+    end
+    possible_horizontal_footprints
+  end
+
+  def conditionally_place_ship(ship, all_combos)
+    has_overlapping = true
+    while has_overlapping == true
+      ship_footprint = all_combos.sample
+      has_overlapping = ship_footprint.any? do |cell|
+        @board.cells[cell].empty? == false
+      end
+    end
+    @board.place(ship, ship_footprint)
+  end
+
+
   def feed_ships
     @computer_ships.each do |ship|
-
-      possible_horizontal_footprints = []
-      @board.row_combos(ship).each do |row_combo|
-        row_combo.each do |row|
-          @board.column_combos(ship).each do |col_combo|
-            possible_ship_footprint = col_combo.map do |col|
-              row + col
-            end
-            possible_horizontal_footprints << possible_ship_footprint
-            possible_ship_footprint = []
-          end
-        end
-      end
-
-      all_combos = (possible_horizontal_footprints + possible_vertical_footprints(ship)).uniq
-
-      has_overlapping = true
-      while has_overlapping == true
-        ship_footprint = all_combos.sample
-        has_overlapping = ship_footprint.any? do |cell|
-          @board.cells[cell].empty? == false
-        end
-      end
-      @board.place(ship, ship_footprint)
+      all_combos = (possible_horizontal_footprints(ship) + possible_vertical_footprints(ship)).uniq
+      conditionally_place_ship(ship,all_combos)
     end
   end
 
