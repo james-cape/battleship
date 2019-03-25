3ebcfaf5060c906c2eaec5e13c3f7390163cd36a
James Cape <james.cape@gmail.com>
2019-03-25 16:33:59 -0600

    Implement possible_vertical_footprints helper method

--- lib/computer.rb
+++ lib/computer.rb
@@ -21,20 +21,21 @@ class Computer
         end
       end
 
-      possible_vertical_footprints = []
-      @board.column_combos(ship).each do |col_combo|
-        col_combo.each do |col|
-          @board.row_combos(ship).each do |row_combo|
-            possible_ship_footprint = row_combo.map do |row|
-              row + col
-            end
-            possible_vertical_footprints << possible_ship_footprint
-            possible_ship_footprint = []
-          end
-        end
-      end
 
-      all_combos = (possible_horizontal_footprints + possible_vertical_footprints).uniq
+      # possible_vertical_footprints = []
+      # @board.column_combos(ship).each do |col_combo|
+      #   col_combo.each do |col|
+      #     @board.row_combos(ship).each do |row_combo|
+      #       possible_ship_footprint = row_combo.map do |row|
+      #         row + col
+      #       end
+      #       possible_vertical_footprints << possible_ship_footprint
+      #       possible_ship_footprint = []
+      #     end
+      #   end
+      # end
+
+      all_combos = (possible_horizontal_footprints + possible_vertical_footprints(ship)).uniq
 
       has_overlapping = true
       while has_overlapping == true
@@ -46,4 +47,22 @@ class Computer
       @board.place(ship, ship_footprint)
     end
   end
+
+  def possible_vertical_footprints(ship)
+    return_value = []
+    @board.column_combos(ship).each do |col_combo|
+      col_combo.each do |col|
+        @board.row_combos(ship).each do |row_combo|
+          possible_ship_footprint = row_combo.map do |row|
+            row + col
+          end
+          return_value << possible_ship_footprint
+          possible_ship_footprint = []
+        end
+      end
+    end
+
+
+  end
+
 end
