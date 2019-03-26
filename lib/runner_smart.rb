11b10db51cd3da2990c14919ec53f667119ddb0d
Ryan Miller <ryanmillergm@gmail.com>
2019-03-26 13:14:50 -0600

    Write questions for feedback from Norm

--- lib/runner_smart.rb
+++ lib/runner_smart.rb
@@ -10,3 +10,21 @@ require 'pry'
 @gameplay = Gameplay.new
 
 @gameplay.start
+
+
+# Q for Norm:
+
+# How to write tests for gameplay.rb?
+
+# How to further clean up code smells?
+
+# Please critique our class/object logic?
+
+# Please critique our overall structure in general?
+
+# Any known pitfalls? Such as rendering 1 2 3 4 5 6 7 8 9 10 11 12 13 14 etc
+
+
+
+
+# 
