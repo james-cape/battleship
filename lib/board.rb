b9af9dbb99feef83e2eb1d2a4aa190e9b3d5e5da
Ryan Miller <ryanmillergm@gmail.com>
2019-03-26 16:53:55 -0600

    refactor

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,13 @@
 class Board
-  attr_reader :cells, :rows, :columns
+  attr_reader :cells,
+              :rows,
+              :columns
 
   def initialize(height = 4, width = 4)
     @rows = ("A"..(height.to_i + 64).chr).to_a
     @columns = ("1"..width.to_s).to_a
 #decide on max
     @cells = {}
-    #REPETITIVE
     @rows.each do |row|
       @columns.each do |column|
         @cells[(row + column.to_s)] = Cell.new(row + column.to_s)
@@ -14,17 +15,13 @@ class Board
     end
   end
 
-  def valid_coordinate?(coordinate_array)
-    @cells.keys.include?(coordinate_array)
-  end
-
-  def column_combos(ship)
+  def ship_column_combos(ship)
     @columns.each_cons(ship.length).map do |combo|
       combo
     end
   end
 
-  def row_combos(ship)
+  def ship_row_combos(ship)
     @rows.each_cons(ship.length).map do |combo|
       combo.map do |number|
         number.chr
@@ -33,30 +30,32 @@ class Board
   end
 
   def valid_placement?(ship, coordinate_array)
-    column_combos = column_combos(ship)
-    row_combos = row_combos(ship)
-
-    ship_columns = coordinate_array.map do |coordinate|
-      coordinate.scan(/\d+|\D+/)[1]
-    end
 
     ship_rows = coordinate_array.map do |coordinate|
       coordinate.scan(/\d+|\D+/)[0]
     end
 
-    columns_match = column_combos.any? do |column_combo|
-      column_combo == ship_columns
-    end
-
-    rows_match = row_combos.any? do |row_combo|
+    rows_match = ship_row_combos(ship).any? do |row_combo|
       row_combo == ship_rows
     end
+
+    ship_columns = coordinate_array.map do |coordinate|
+      coordinate.scan(/\d+|\D+/)[1]
+    end
+
+    columns_match = ship_column_combos(ship).any? do |ship_column_combo|
+      ship_column_combo == ship_columns
+    end
+
 
     check_conditionals(ship, coordinate_array, rows_match, columns_match)
   end
 
+  def valid_coordinate?(coordinate_array)
+    @cells.keys.include?(coordinate_array)
+  end
+
   def check_conditionals(ship, coordinate_array, rows_match, columns_match)
-    #Too complicated/Refactor doing 2 jobs ~
     coordinate_array.length == ship.length &&
     ((rows_match == true && columns_match == false) ||
     (rows_match == false && columns_match == true)) &&
@@ -73,7 +72,7 @@ class Board
 
   def render(reveal = false)
     rendered_string = "  "
-    columns.each do |column|
+    @columns.each do |column|
         rendered_string += "#{column.rjust(2,' ')} "
     end
     rendered_string += "\n"
